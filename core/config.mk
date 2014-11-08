1a23048bbd6539ab319b385aca05718e67b69a20
Andrew Dodd <atd7@cornell.edu>
2014-11-08 16:53:21 -0500

    build: Don't use block mode by default, allow it to be chosen
    Currently this uses the same logic as backuptool (but inverted)
    
    i.e. block mode will be disabled in all cases where backuptool is enabled
    which is the current default for all Omni builds.
    
    Block mode is not needed now, and may never be needed for custom projects,
    as block mode is preparation for dm-verity, which is mostly pointless on
    devices with unlocked bootloaders since the kernel already has broken
    the chain of trust.
    
    TODO:  Determine if Nexus 9 *requires* blockmode.  If it does, this
    may need to be reworked to allow the device to specify blockmode.
    
    Change-Id: I36725b44f4066a43ec7e1615bd4b072f7eb3db28

--- core/config.mk
+++ core/config.mk
@@ -627,6 +627,9 @@ else
   DEFAULT_SYSTEM_DEV_CERTIFICATE := build/target/product/security/testkey
 endif
 
+# Rules for QCOM targets
+include $(BUILD_SYSTEM)/qcom_target.mk
+
 # ###############################################################
 # Set up final options.
 # ###############################################################
@@ -867,16 +870,11 @@ endif
 RSCOMPAT_32BIT_ONLY_API_LEVELS := 8 9 10 11 12 13 14 15 16 17 18 19 20
 RSCOMPAT_NO_USAGEIO_API_LEVELS := 8 9 10 11 12 13
 
-<<<<<<< HEAD
-=======
-# Rules for QCOM targets
-include $(BUILD_SYSTEM)/qcom_target.mk
 
 ifneq ($(CUSTOM_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/omni/sepolicy/sepolicy.mk)
+$(eval include vendor/N-7.1.1_r7/sepolicy/sepolicy.mk)
 endif
 
->>>>>>> c82baf58c... build: Add QCOM target-specific config
 include $(BUILD_SYSTEM)/dumpvar.mk
